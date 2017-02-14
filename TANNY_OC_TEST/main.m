e0bd836e9c06d5a290899433f4c50788b908f508
tanny <tanorigin@gmail.com>
2017-02-14 23:28:36 +0800

    Initial Commit

---
+++ TANNY_OC_TEST/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TANNY_OC_TEST
+//
+//  Created by tanny wong on 2017/2/14.
+//  Copyright © 2017年 tanny wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
