783716a76534888aa5fffb71c60fbc925cb59e24
tanny <tanorigin@gmail.com>
2017-02-17 22:33:08 +0800

    update .gitignore

---
+++ 7.KVO/TwoTimesArray.h
@@ -0,0 +1,16 @@
+//
+//  TwoTimesArray.h
+//  TANNY_OC_TEST
+//
+//  Created by tanny wong on 2017/2/17.
+//  Copyright © 2017年 tanny wong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TwoTimesArray : NSObject
+-(void)incrementCount;
+-(NSUInteger)countOfNumbers;
+-(id)objectInNumbersAtIndex:(NSUInteger)index;
+
+@end
